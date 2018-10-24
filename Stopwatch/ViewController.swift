77c3d3cb0d4e18ebb640fe16a75d3ccbb6c79c09
Kevin Wang <kwangsan@gmail.com>
2018-10-24 15:19:57 -0400

    Initial Commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Kevin Wang on 10/24/18.
+//  Copyright © 2018 Kevin Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+

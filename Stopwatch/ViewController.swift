baa110fd23f98edf13320260c5256b43834eea8b
Kevin Wang <kwangsan@gmail.com>
2018-10-24 16:36:14 -0400

    Timer; Start/Stop on tap

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+  var timer: Timer!
+  var startButton: UILabel!
+
+  var count: Double!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    startButton = UILabel()
+
+    count = 0.0
+
+    setupViews()
+    setupStartButton()
   }
 
+  func setupViews() {
+    view.backgroundColor = .white
+    view.addSubview(startButton)
 
+    startButton.text = String(count)
+    startButton.textAlignment = .center
+    startButton.snp.makeConstraints { make in
+      make.centerX.centerY.equalTo(view)
+      make.width.height.equalTo(100)
+    }
+  }
+
+  func setupStartButton() {
+    let tapped = UITapGestureRecognizer(target: self, action: #selector(tap))
+    startButton.addGestureRecognizer(tapped)
+    startButton.isUserInteractionEnabled = true
+  }
+
+  @objc func tap() {
+    if let _ = timer {
+      timer.invalidate()
+      timer = Timer()
+    } else {
+      timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { timer in
+        self.count += timer.timeInterval
+        print("Count == \(self.count!)")
+        self.startButton.text = String(format: "%.1f", self.count)
+      })
+      self.startButton.backgroundColor = .gray
+    }
+  }
 }
 
